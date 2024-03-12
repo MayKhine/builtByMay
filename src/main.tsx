0e1c8dd9b434e7b59b5ad7cde564649c3d183992
May <maykhine.ada@gmail.com>
2024-03-12 15:27:53 -0400

    add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Home } from "./Home.tsx"
+import { Projects } from "./pages/Projects.tsx"
+import "./index.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Home />} path="/"></Route>
+        <Route element={<Projects />} path="/projects"></Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
