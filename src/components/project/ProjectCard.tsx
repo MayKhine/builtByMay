b74ba31438f33f411451df5051f5fa094e8926d7
May <maykhine.ada@gmail.com>
2024-03-16 13:58:07 -0400

    add another project data for test

--- src/components/project/ProjectCard.tsx
+++ src/components/project/ProjectCard.tsx
@@ -1,8 +1,9 @@
 import * as styleX from "@stylexjs/stylex"
-
+import { tokens } from "../../tokens.stylex"
 export type ProjectType = {
   title: string
   text: string
+  link: string
 }
 
 type ProjectCardProps = {
@@ -12,12 +13,37 @@ type ProjectCardProps = {
 export const ProjectCard = ({ projectData }: ProjectCardProps) => {
   return (
     <div {...styleX.props(styles.base)}>
-      {projectData.title}
+      <div>
+        <a
+          href={projectData.link}
+          target="_blank"
+          {...styleX.props(styles.title)}
+        >
+          {projectData.title}
+        </a>
+      </div>
+
       <div> {projectData.text}</div>
     </div>
   )
 }
 
 const styles = styleX.create({
-  base: { backgroundColor: "blue" },
+  base: {
+    minWidth: "300px",
+    minHeight: "300px",
+    width: "30%",
+    height: "30%",
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: tokens.darkBlue2,
+    color: tokens.offWhite,
+    margin: "5px",
+  },
+  title: {
+    fontWeight: "600",
+    cursor: "pointer",
+    color: tokens.offWhite,
+    textDecorationLine: "none",
+  },
 })
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,26 @@
 import * as stylex from "@stylexjs/stylex"
 import { tokens } from "../tokens.stylex"
-import { MenuBar } from "../components/MenuBar"
+// import { MenuBar } from "../components/MenuBar"
 import { ProjectCard, ProjectType } from "../components/project/ProjectCard"
 
 export const Home = () => {
-  const project1: ProjectType = { title: "TEST", text: "TEST TEST TEST" }
+  const project1: ProjectType = {
+    title: "Old Portfolio Project",
+    text: "Github pages",
+    link: "https://maykhine.github.io/portfolio/",
+  }
+
+  const project2: ProjectType = {
+    title: "Old Todo Project",
+    text: "Another Github pages",
+    link: "https://maykhine.github.io/todo-app/",
+  }
   return (
-    <div>
-      <MenuBar />
-      <div {...stylex.props(styles.base)}>Helloooo</div>
+    <div {...stylex.props(styles.base)}>
+      {/* <MenuBar /> */}
+      <div>HELLO HOME PAGE</div>
       <ProjectCard projectData={project1} />
+      <ProjectCard projectData={project2} />
     </div>
   )
 }
@@ -17,9 +28,10 @@ export const Home = () => {
 const styles = stylex.create({
   base: {
     // maxHeight: "100vh",
-    // height: "100vh",
+    // marginTop: "30px",
+    height: "100vh",
     // backgroundColor: "blue",
-    backgroundColor: tokens.darkBlue2,
+    backgroundColor: tokens.offWhite,
 
     // backgroundColor: colors.white,
   },
